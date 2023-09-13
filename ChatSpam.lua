92a9ac4e880579bb2f512545f93fc5dba853159f
Camryn <76259034+DysphoricEnby@users.noreply.github.com>
2023-09-13 02:55:28 -0400

    Create ChatSpam.lua

---
+++ ChatSpam.lua
@@ -0,0 +1,39 @@
+-- Check if an argument for the chat hostname was provided
+local args = { ... }
+local chatHostname = args[1]
+
+if not chatHostname then
+  print("Usage: ChatSpam.lua <chat_hostname>")
+  return
+end
+
+-- Function to generate a random 16-character string
+local function generateRandomName()
+  local chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  local name = ""
+  for i = 1, 16 do
+    local randomIndex = math.random(#chars)
+    name = name .. chars:sub(randomIndex, randomIndex)
+  end
+  return name
+end
+
+-- Initialize the Wired modem on each remote computer
+rednet.open("top") -- Change the side as needed
+
+-- Generate a random chat name for this computer
+local chatName = generateRandomName()
+
+-- Join the chat using the provided hostname and random name
+rednet.chat(chatHostname)
+rednet.broadcast(chatName .. " has joined the chat.")
+
+-- Main loop for chatting
+while true do
+  local senderID, message, protocol = rednet.receive()
+  if senderID and message then
+    -- Print the received message with the sender's chat name
+    local senderName = generateRandomName() -- Generate a random sender name for privacy
+    print(senderName .. " (" .. senderID .. "): " .. message)
+  end
+end
