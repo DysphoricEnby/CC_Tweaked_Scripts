a9196773148c31a0168aeb95cc5151745b43775b
Camryn <76259034+DysphoricEnby@users.noreply.github.com>
2023-09-13 02:46:44 -0400

    Update CC_Zombie_Script.lua

--- CC_Zombie_Script.lua
+++ CC_Zombie_Script.lua
@@ -1,23 +1,14 @@
 -- Initialize the Wired modem on each remote computer
-peripheral.find("modem").open(1) -- Change the side (1) as needed
-
--- Get the ID of the current computer
-local ownID = os.getComputerID()
+rednet.open("back") -- Change the side as needed
 
 -- Main loop for receiving and executing commands
 while true do
-  local _, senderID, _, _, _, message = os.pullEvent("modem_message")
-  if senderID and message and senderID ~= ownID then
-    -- Execute standard ComputerCraft commands received from the central server
-    local success, output = pcall(function()
-      return shell.run(message)
-    end)
-
-    -- Display the output locally on the remote computer
-    print("Output of Command:")
-    print(output)
-
-    -- Optionally, you can add a sleep time to introduce a delay before listening for the next command
-    sleep(5) -- Adjust the delay time as needed
+  local senderID, message, protocol = rednet.receive()
+  if senderID and message then
+    -- Execute the received command (assuming it's a valid CC command)
+    local success, result = pcall(shell.run, message)
+    
+    -- Send the result (if any) back to the central server
+    rednet.send(senderID, result or "")
   end
 end
