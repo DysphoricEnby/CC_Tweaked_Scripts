f99388a9acafcd6ebe99285ce00c667d15404304
Camryn <76259034+DysphoricEnby@users.noreply.github.com>
2023-09-13 02:58:35 -0400

    Create CC_Zombie_Setup.lua

---
+++ CC_Zombie_Setup.lua
@@ -0,0 +1,30 @@
+-- List of files to copy to the root directory
+local filesToCopy = {
+  "disk/startup.file",
+  "disk/ChatSpam.lua", -- Add any other files you want to copy here
+}
+
+-- Function to copy a file
+local function copyFile(source, destination)
+  if fs.exists(source) then
+    fs.copy(source, destination)
+    print("Copied '" .. source .. "' to '" .. destination .. "'")
+  else
+    print("File '" .. source .. "' does not exist.")
+  end
+end
+
+-- Delete the existing "startup" file if it exists
+if fs.exists("startup") then
+  fs.delete("startup")
+  print("Deleted existing 'startup' file")
+end
+
+-- Copy the specified files to the root directory
+for _, file in ipairs(filesToCopy) do
+  local fileName = fs.getName(file)
+  copyFile(file, fileName)
+end
+
+-- Run the new "startup" file
+shell.run("startup")
